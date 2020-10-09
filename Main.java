56f5bfe6866abeaf09f79b8facdd1308cfcfdca4
Gormi3815 <gormi3815@wrdsb.ca>
2020-10-09 14:58:22 +0000

    the program will find thelargest integer out of the 10 integers provided

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
-/**
+import java.util.Scanner;
+/** the program will allow the user to enter 10 integers and then find the largest integer out of the group
  *
- * @author 
+ * @author Isaac
  */
 public class Main {
 
@@ -10,6 +11,20 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    int arnum = 10;
+    int[] num =new int[arnum];
+    System.out.println("Please enter 10 integers");
+    for(int i =0; i < num.length; i++){
+    num[i] = input.nextInt();
+    }
+    int lgnum = num[0];
+    for(int i=0; i < num.length; i++){
+      if (num[i] > lgnum){
+        lgnum = num[i];
+      }
+      }
+      System.out.println("The largest number is " + lgnum);
+    }
   }
-}
+
